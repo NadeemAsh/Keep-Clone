41d2641c0f821038a31ba15cc54c670b073aef3c
Nadeem Ashraf <dev.nad.ash@gmail.com>
2023-03-18 13:32:46 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Google Keep Clone</h1>
+      <h4>But with some minor modifications</h4>
+    </div>
+  )
+}
+
+export default App
